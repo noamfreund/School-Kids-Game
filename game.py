1aea782e6d210c5324bc63df59d72c176f5f61c0
noamf <noamfreund89@gmail.com>
2022-12-27 14:35:33 +0200

    updated

--- game.py
+++ game.py
@@ -2,26 +2,6 @@ import os
 import time
 import tkinter as tk
 
-"""
-here are the 2 keys for each riddle
-
-1. (שבט צופים אחים ואחיות)
-a. you start with gimatria in hebrew 
-b. you shift each letter 3 letters to the rigth for example ד is א
-
-[{3,9,40,20,4,9},{70,40,20,4},{70,40,200,9,300},{30,5,2}]   gimatria
-ב ה  ל     ש  ט  ר  מ   ע   ד  כ  מ  ע    ט ד כ  מ  ט ג  
-ש ב  ט     צ  ו  פ  י   ם   א  ח  י  ם    ו א ח  י  ו ת  
-
-2. (מדעי המחשב ללא מחשב)
-a. you start with gimatria in hebrew 
-b. you shift first letter 1 then second 2 and on and on always to the right
-
-[{8,6,70,400},{20,1,2},{60,40,1,7,400},{6,40,2,30}]   gimatria
-ל ב  מ  ו    ת  ז מ  א  ס   ב  א כ     ת  ע  ו ח  
-מ ד  ע  י    ה  מ ח  ש  ב   ל  ל א     מ  ח  ש ב  
-
-"""
 
 
 def play_song_button_clicked1():
